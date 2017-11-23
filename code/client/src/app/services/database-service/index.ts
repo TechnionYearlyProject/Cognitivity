b3f39a4606d9734b642dd843117eec124b76791e
Daniel Lyubin <dani9590@gmail.com>
2017-11-23 10:32:26 +0200

    Closed #1
    Added dependencies and organized a project structure. Follow README
    files when adding a component/service. When adding a data object
    interface, add to src/app/models/index.ts.

---
+++ code/client/src/app/services/database-service/index.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class TestManagerService {
+    //functions
+    constructor() {}
+}
+
+@Injectable()
+export class SubjectService {
+    //functions
+    constructor() {}
+}
+
+@Injectable()
+export class TestService {
+    //functions
+    constructor() {}
+}
+
+@Injectable()
+export class QuestionService {
+    //functions
+    constructor() {}
+}
+
+@Injectable()
+export class TestAnswerService {
+    //functions
+    constructor() {}
+}
