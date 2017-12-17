bf9c9b102c825a31f2bddb90bddde17fa5f5daba
guy <guyshe@campus.technion.ac.il>
2017-12-17 14:15:01 +0200

    execute query dao

---
+++ code/src/main/java/cognitivity/dao/ExecuteQuery.java
@@ -0,0 +1,32 @@
+package cognitivity.dao;
+
+
+import javax.persistence.*;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.HibernateException;
+
+
+public class ExecuteQuery() {
+
+    List execute(String query){
+        Session session = factory.openSession();
+        Transaction tx = null;
+
+        try{
+            tx=session.beginTransaction();
+            SQLQuery query=session.createSQLQuery(query);
+            query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+            List data=query.list();
+        } catch(HibernateException e) {
+            if( tx != null)
+                tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
