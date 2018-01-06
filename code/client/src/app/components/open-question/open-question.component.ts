aba48864cea488f3ceb0626e8b2b8d7f82110efa
markerlich <markerlich@github.com>
2018-01-06 21:52:27 +0200

    finshed the view of all types of questions closed #28 closed #29 closed #30

--- code/client/src/app/components/open-question/open-question.component.ts
+++ code/client/src/app/components/open-question/open-question.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { OpenQuestion, TypeQuestion } from '../../models';
+import { OpenQuestion, TypeQuestion, QuestionPosition } from '../../models';
 
 @Component({
   selector: 'app-open-question',
@@ -10,15 +10,20 @@ export class OpenQuestionComponent implements OnInit {
   question: OpenQuestion;
   currentAnswer: string;
   range_value: number = 50;
+  positionUp : any;
+  positionMiddle : any;
+  positionButtom : any;
   constructor() { 
     /*
     hardcoded question object, when services will be added this piece of code will not be needed
     */ 
     this.question = {
       questionText: 'Which one is better, Iphone or Galaxy?',
+      questionPosition : QuestionPosition.UpperMiddle,
       type: TypeQuestion.OpenQuestion,
       answerText: 'Iphone'
     };
+    this.buildPositionOfQuestion();
     //End of hardcoded question
   }
 
@@ -28,5 +33,64 @@ export class OpenQuestionComponent implements OnInit {
   onSubmit(event: Event){
     //this.currentAnswer = answer;
     console.log('The answer is: ' + this.currentAnswer);
+  }
+
+  buildPositionOfQuestion(){
+    this.positionUp = {
+      'right' : this.isUpperRight(),
+      'middle' : this.isUpperMiddle(),
+      'left' : this.isUpperLeft()
+    }
+    this.positionButtom = {
+      'right' : this.isButtomRight(),
+      'middle' : this.isButtomMiddle(),
+      'left' : this.isButtomLeft()
+    }
+    this.positionMiddle = {
+      'right' : this.isMiddleRight(),
+      'middle' : this.isMiddleMiddle(),
+      'left' : this.isMiddleLeft()
+    }
+  }
+
+
+   /*
+    ------------------------------- functions for asking the position of the question text -----------------------
+  */
+  isUpperMiddle(): boolean{
+    return this.question.questionPosition == QuestionPosition.UpperMiddle;
+  }
+  isUpperRight(): boolean{
+    return this.question.questionPosition == QuestionPosition.UpperRight;
+  }
+  isUpperLeft(): boolean{
+    return this.question.questionPosition == QuestionPosition.UpperLeft;
+  }
+  isButtomRight(): boolean{
+    return this.question.questionPosition == QuestionPosition.ButtomRight;
+  }
+  isButtomMiddle(): boolean{
+    return this.question.questionPosition == QuestionPosition.ButtomMiddle;
+  }
+  isButtomLeft(): boolean{
+    return this.question.questionPosition == QuestionPosition.ButtomLeft;
+  }
+  isMiddleRight(): boolean{
+    return this.question.questionPosition == QuestionPosition.MiddleRight;
+  }
+  isMiddleMiddle(): boolean{
+    return this.question.questionPosition == QuestionPosition.MiddleMiddle;
+  }
+  isMiddleLeft(): boolean{
+    return this.question.questionPosition == QuestionPosition.MiddleLeft;
+  }
+  isUp(): boolean{
+    return this.isUpperMiddle() || this.isUpperRight() || this.isUpperLeft();
+  }
+  isMiddle(): boolean{
+    return this.isMiddleMiddle() || this.isMiddleRight() || this.isMiddleLeft();
+  }
+  isButtom():boolean{
+    return this.isButtomMiddle() || this.isButtomRight() || this.isButtomLeft();
   }
 }
--- code/client/src/app/components/rate-question/rate-question.component.ts
+++ code/client/src/app/components/rate-question/rate-question.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { RateQuestion, MultipleAnswer, TypeQuestion } from '../../models';
+import { Component, OnInit, AnimationStyleMetadata } from '@angular/core';
+import { RateQuestion, MultipleAnswer, TypeQuestion, QuestionPosition } from '../../models';
 import { stringify } from '@angular/core/src/util';
 @Component({
   selector: 'app-rate-question',
@@ -11,16 +11,21 @@ export class RateQuestionComponent implements OnInit {
   answers: Array<MultipleAnswer>;
   markedAnswer : number;
   range_value : number = 50;
+  positionUp : any;
+  positionMiddle : any;
+  positionButtom : any;
   constructor() {
     /*
     hardcoded question object, when services will be added this piece of code will not be needed
     */ 
       this.question = {
         questionText: 'How is your social life?',
+        questionPosition: QuestionPosition.UpperMiddle,
         type: TypeQuestion.RateQuestion,
         heightOfRate: 5
       }
       //End of harcoded question
+      this.buildPositionOfQuestion();
       this.answers = new Array<MultipleAnswer>(this.question.heightOfRate);
       for(let i = 0; i < this.question.heightOfRate; i++){
         this.answers[i] = {answer: (i + 1).toString(), isMarked: false};
@@ -42,6 +47,64 @@ export class RateQuestionComponent implements OnInit {
     }
   }
 
+  buildPositionOfQuestion(){
+    this.positionUp = {
+      'right' : this.isUpperRight(),
+      'middle' : this.isUpperMiddle(),
+      'left' : this.isUpperLeft()
+    }
+    this.positionButtom = {
+      'right' : this.isButtomRight(),
+      'middle' : this.isButtomMiddle(),
+      'left' : this.isButtomLeft()
+    }
+    this.positionMiddle = {
+      'right' : this.isMiddleRight(),
+      'middle' : this.isMiddleMiddle(),
+      'left' : this.isMiddleLeft()
+    }
+  }
+
+
+   /*
+    ------------------------------- functions for asking the position of the question text -----------------------
+  */
+  isUpperMiddle(): boolean{
+    return this.question.questionPosition == QuestionPosition.UpperMiddle;
+  }
+  isUpperRight(): boolean{
+    return this.question.questionPosition == QuestionPosition.UpperRight;
+  }
+  isUpperLeft(): boolean{
+    return this.question.questionPosition == QuestionPosition.UpperLeft;
+  }
+  isButtomRight(): boolean{
+    return this.question.questionPosition == QuestionPosition.ButtomRight;
+  }
+  isButtomMiddle(): boolean{
+    return this.question.questionPosition == QuestionPosition.ButtomMiddle;
+  }
+  isButtomLeft(): boolean{
+    return this.question.questionPosition == QuestionPosition.ButtomLeft;
+  }
+  isMiddleRight(): boolean{
+    return this.question.questionPosition == QuestionPosition.MiddleRight;
+  }
+  isMiddleMiddle(): boolean{
+    return this.question.questionPosition == QuestionPosition.MiddleMiddle;
+  }
+  isMiddleLeft(): boolean{
+    return this.question.questionPosition == QuestionPosition.MiddleLeft;
+  }
+  isUp(): boolean{
+    return this.isUpperMiddle() || this.isUpperRight() || this.isUpperLeft();
+  }
+  isMiddle(): boolean{
+    return this.isMiddleMiddle() || this.isMiddleRight() || this.isMiddleLeft();
+  }
+  isButtom():boolean{
+    return this.isButtomMiddle() || this.isButtomRight() || this.isButtomLeft();
+  }
   onSubmit(evet: Event){
     
   }
