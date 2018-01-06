4de808bea0949d80862851bb91d65450b08aa76d
Ben Hunter <benhunt96@campus.technion.ac.il>
2018-01-06 15:20:29 +0200

    issue #77 closed

--- code/client/src/app/components/block/question-list/question-list.component.ts
+++ code/client/src/app/components/block/question-list/question-list.component.ts
@@ -15,13 +15,36 @@ export class QuestionListComponent implements OnInit {
 
   ngOnInit() {
     this.questions[0]=new QuestionComponent();
+    this.questions[0].indexInBlock=0;
+    this.questions[0].questionName="i am 1";
     this.questions[0].type=0;
 
     this.questions[1]=new QuestionComponent();
+    this.questions[1].indexInBlock=1;
+    this.questions[1].questionName="i am 2";
     this.questions[1].type=1;
 
     this.questions[2]=new QuestionComponent();
+    this.questions[2].indexInBlock=2;
+    this.questions[2].questionName="i am 3";
     this.questions[2].type=2;
   }
 
+  moveMeUp(currentIndex)
+  {
+    this.moveElementInArray(this.questions,this.questions[currentIndex],currentIndex,currentIndex+1);
+  }
+
+  moveMeDown(currentIndex)
+  {
+    this.moveElementInArray(this.questions,this.questions[currentIndex],currentIndex,currentIndex-1);
+  }
+
+  moveElementInArray(array,myCurrContent,myCurrIndex , myWantedIndex)
+{
+  array.splice(myCurrIndex,1);
+  array.splice(myWantedIndex,0,myCurrContent); 
 }
+}
+
+
