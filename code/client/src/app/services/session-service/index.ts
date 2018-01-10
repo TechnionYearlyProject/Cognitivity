cd07e0c93e03b35ceb8625bd1fba69806da3d05d
Daniel Lyubin <dani9590@gmail.com>
2018-01-10 17:04:30 +0200

    Closed #90

--- code/client/src/app/services/session-service/index.ts
+++ code/client/src/app/services/session-service/index.ts
@@ -2,6 +2,15 @@
    without feching it again from the DB
  */
 
+/*
+  Usage example:
+    At the constructor of the component, declare a variable of SessionService with the desired type (let's say Test). 
+    When routing from component A to component B and wishing to pass a Test object,
+    at the click event of the button that does the routing, call this.sessionService.setData(test) where 'test' is the object.
+    In component B, at ngOnInit, save the object returned from this.sessionService.getData() .
+    If you wish to clear the object in the service, call this.sessionService.clearData(),
+    and remember to check with isNull if data is available! */
+
 import { Injectable } from '@angular/core';
 
 @Injectable()
