aa3d7d515e8371b5bcf8415bda6ce95bdf154310
Guy Shemesh <guyshe@campus.technion.ac.il>
2018-01-12 18:34:29 +0200

    closed #100

--- code/src/main/java/cognitivity/dao/TestQuestionDAO.java
+++ code/src/main/java/cognitivity/dao/TestQuestionDAO.java
@@ -2,26 +2,15 @@ package cognitivity.dao;
 
 import cognitivity.entities.TestManager;
 import cognitivity.entities.TestQuestion;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-/**
- * Data Access Object for TestQuestion object
- */
-@Repository
-public class TestQuestionDAO extends AbstractDAO<TestQuestion> {
+public interface TestQuestionDAO {
 
-    public TestQuestion get(Long id) {
-        return super.get(id, TestQuestion.class);
-    }
-
-    public void delete(Long id) {
-        super.delete(id, TestQuestion.class);
-    }
+    public TestQuestion get(Long id);
+    public void delete(Long id);
+    public void add(TestQuestion data);
+    public void update(TestQuestion data);
 
     /**
      * Get all test questions from a given manager.
@@ -29,12 +18,6 @@ public class TestQuestionDAO extends AbstractDAO<TestQuestion> {
      * @param manager - The manager from which we want to get the questions
      * @return - A list of all test questions in the test
      */
-    @Transactional
-    public List<TestQuestion> getTestQuestionsFromAManager(TestManager manager) {
-        Session session = sessionFactory.getCurrentSession();
-        String queryString = "from TestQuestion where testManager = :manager";
-        Query<TestQuestion> query = session.createQuery(queryString, TestQuestion.class);
-        query.setParameter("manager", manager);
-        return query.getResultList();
-    }
+    public List<TestQuestion> getTestQuestionsFromAManager(TestManager manager);
+
 }
---
+++ code/src/main/java/cognitivity/dao/TestQuestionDAOimpl.java
@@ -0,0 +1,35 @@
+package cognitivity.dao;
+
+import cognitivity.entities.TestManager;
+import cognitivity.entities.TestQuestion;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Data Access Object for TestQuestion object
+ */
+@Repository
+public class TestQuestionDAOimpl extends AbstractDAO<TestQuestion> implements TestQuestionDAO{
+
+    public TestQuestion get(Long id) {
+        return super.get(id, TestQuestion.class);
+    }
+
+    public void delete(Long id) {
+        super.delete(id, TestQuestion.class);
+    }
+
+    @Transactional
+    public List<TestQuestion> getTestQuestionsFromAManager(TestManager manager) {
+        Session session = sessionFactory.getCurrentSession();
+        String queryString = "from TestQuestion where testManager = :manager";
+        Query<TestQuestion> query = session.createQuery(queryString, TestQuestion.class);
+        query.setParameter("manager", manager);
+        List<TestQuestion> res =  query.getResultList();
+        return res;
+    }
+}
