95462a5f24dd61f1940dfa8c519cb4235a0fb866
Ben Hunter <benhunt96@campus.technion.ac.il>
2018-01-12 17:48:49 +0200

    issue #95 fixed

--- code/client/src/app/components/block/question-list/question-list.component.ts
+++ code/client/src/app/components/block/question-list/question-list.component.ts
@@ -9,42 +9,63 @@ import { TypeQuestion } from '../../../models/index';
 })
 export class QuestionListComponent implements OnInit {
 
-  questions:QuestionComponent[]=new Array();
+  questions: QuestionComponent[] = new Array();
 
   constructor() { }
 
   ngOnInit() {
-    this.questions[0]=new QuestionComponent();
-    this.questions[0].indexInBlock=0;
-    this.questions[0].questionName="i am 1";
-    this.questions[0].type=0;
+    this.questions[0] = new QuestionComponent();
+    this.questions[0].indexInBlock = 0;
+    this.questions[0].questionName = "i am 1";
+    this.questions[0].type = 0;
 
-    this.questions[1]=new QuestionComponent();
-    this.questions[1].indexInBlock=1;
-    this.questions[1].questionName="i am 2";
-    this.questions[1].type=1;
+    this.questions[1] = new QuestionComponent();
+    this.questions[1].indexInBlock = 1;
+    this.questions[1].questionName = "i am 2";
+    this.questions[1].type = 1;
 
-    this.questions[2]=new QuestionComponent();
-    this.questions[2].indexInBlock=2;
-    this.questions[2].questionName="i am 3";
-    this.questions[2].type=2;
+    this.questions[2] = new QuestionComponent();
+    this.questions[2].indexInBlock = 2;
+    this.questions[2].questionName = "i am 3";
+    this.questions[2].type = 2;
   }
 
-  moveMeUp(currentIndex)
-  {
-    this.moveElementInArray(this.questions,this.questions[currentIndex],currentIndex,currentIndex+1);
+  moveMeUp(currentIndex) {
+    let arrayLastIndex = this.questions.length - 1;
+    if (currentIndex == 0) {
+      let tmpMe = this.questions.shift();
+      for (let num = 0; num < arrayLastIndex; num++) {
+        this.questions[num].indexInBlock--;
+      }
+      this.questions.push(tmpMe);
+      tmpMe.indexInBlock = arrayLastIndex;
+    }
+    let tmpHolder = this.questions[currentIndex - 1];
+    let mememe = this.questions[currentIndex];
+    mememe.indexInBlock--;
+    tmpHolder.indexInBlock++;
+    this.questions[currentIndex - 1] = mememe;
+    this.questions[currentIndex] = tmpHolder;
   }
 
-  moveMeDown(currentIndex)
-  {
-    this.moveElementInArray(this.questions,this.questions[currentIndex],currentIndex,currentIndex-1);
+  moveMeDown(currentIndex) {
+    let arrayLastIndex = this.questions.length - 1;
+    if (currentIndex == arrayLastIndex) {
+      let tmpMe = this.questions.pop();
+      for (let num = arrayLastIndex-1; num > 0; num--) {
+        this.questions[num].indexInBlock++;
+      }
+      this.questions.unshift(tmpMe);
+      tmpMe.indexInBlock = 0;
+    }
+    let tmpHolder = this.questions[currentIndex + 1];
+    let mememe = this.questions[currentIndex];
+    mememe.indexInBlock++;
+    tmpHolder.indexInBlock--;
+    this.questions[currentIndex + 1] = mememe;
+    this.questions[currentIndex] = tmpHolder;
   }
 
-  moveElementInArray(array,myCurrContent,myCurrIndex , myWantedIndex)
-{
-  array.splice(myCurrIndex,1);
-  array.splice(myWantedIndex,0,myCurrContent); 
-}
 }
 
 
