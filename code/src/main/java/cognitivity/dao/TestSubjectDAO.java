71eb2d8074f1279bb25f6813215656ef4d3f2051
Guy Shemesh <guyshe@campus.technion.ac.il>
2018-01-12 18:35:29 +0200

    closed #101

--- code/src/main/java/cognitivity/dao/TestSubjectDAO.java
+++ code/src/main/java/cognitivity/dao/TestSubjectDAO.java
@@ -2,25 +2,15 @@ package cognitivity.dao;
 
 import cognitivity.entities.TestAnswer;
 import cognitivity.entities.TestSubject;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-/**
- * Data Access Object for TestQuestion object
- */
-@Repository
-public class TestSubjectDAO extends AbstractDAO<TestSubject> {
-    public TestSubject get(Long id) {
-        return super.get(id, TestSubject.class);
-    }
+public interface TestSubjectDAO {
 
-    public void delete(Long id) {
-        super.delete(id, TestSubject.class);
-    }
+    public TestSubject get(Long id);
+    public void delete(Long id);
+    public void add(TestSubject data);
+    public void update(TestSubject data);
 
     /**
      * Get all the answers for the given test subject
@@ -28,15 +18,7 @@ public class TestSubjectDAO extends AbstractDAO<TestSubject> {
      * @param subjectId - the test subjects Id.
      * @return - A list of all the answers the test subject has given.
      */
-    @Transactional
-    public List<TestAnswer> getSubjectAnswers(long subjectId) {
-        TestSubjectDAO dao = new TestSubjectDAO();
-        Session session = sessionFactory.getCurrentSession();
-        String queryString = "from TestAnswer T where T.testSubject = :testSubject";
-        Query<TestAnswer> query = session.createQuery(queryString, TestAnswer.class);
-        query.setParameter("testSubject", dao.get(subjectId));
-        return query.getResultList();
-    }
+    public List<TestAnswer> getSubjectAnswers(long subjectId);
 
     /**
      * Get all the test subject who participated in a given test.
@@ -45,16 +27,6 @@ public class TestSubjectDAO extends AbstractDAO<TestSubject> {
      * @param testId - the test Id of the given test
      * @return - A list of the subjects who participated the test.
      */
-    @Transactional
-    public List<TestSubject> getTestSubjectsWhoParticipatedInTest(long testId) {
-        Session session = sessionFactory.getCurrentSession();
-        CognitiveTestDAO cognitiveTestDAO = new CognitiveTestDAO();
-        String queryString =
-                "select testSubject " +
-                "from TestSubject testSubject, TestAnswer testAnswer " +
-                "where testSubject.id = testAnswer.id AND testAnswer.CognitiveTest = :cognitiveTest";
-        Query<TestSubject> query = session.createQuery(queryString, TestSubject.class);
-        query.setParameter("cognitiveTest", cognitiveTestDAO.get(testId));
-        return query.getResultList();
-    }
+    public List<TestSubject> getTestSubjectsWhoParticipatedInTest(long testId);
+
 }
