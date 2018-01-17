280d06055e6406f56244af9666dff8e4956a6078
peer-sagiv <peer.sagiv@gmail.com>
2018-01-18 01:39:38 +0200

    Edited test Manager Test

--- code/src/test/java/cognitivity/entities/TestManagerTest.java
+++ code/src/test/java/cognitivity/entities/TestManagerTest.java
@@ -1,6 +1,7 @@
 package cognitivity.entities;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by ophir on 15/01/18.
@@ -10,5 +11,13 @@ public class TestManagerTest {
     @Test
     public void gettersSettersTest() {
 
+        TestManager manager = new TestManager();
+        assertEquals("Problem with creating empty manager",null, manager.getEmail());
+        manager.setEmail("BlaBla@bla.com");
+        assertEquals("Problem with setting email", "BlaBla@bla.com", manager.getEmail());
+
+        TestManager manager1 = new TestManager("RealEmail@FakeEmail.com");
+
+        assertEquals("Problem with creating a test manager with given Email", manager1.getEmail(), "RealEmail@FakeEmail.com");
     }
 }
