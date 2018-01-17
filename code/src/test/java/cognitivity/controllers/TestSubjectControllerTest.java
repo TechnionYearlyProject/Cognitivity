868430e34ce34cf249bdbe5679627f683c22be9b
ophirKatz <ophirk8396@gmail.com>
2018-01-18 01:45:17 +0200

    updating logger

--- code/src/test/java/cognitivity/controllers/TestSubjectControllerTest.java
+++ code/src/test/java/cognitivity/controllers/TestSubjectControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -33,7 +32,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {TestContextBeanConfiguration.class, HibernateBeanConfiguration.class})
-@WebAppConfiguration
 public class TestSubjectControllerTest implements RestControllerTest {
 
     private TestSubjectController controller;
