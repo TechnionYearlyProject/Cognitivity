4b3a314333b5adf7c5c21d490fe178e27d17b8ac
ophirKatz <ophirk8396@gmail.com>
2018-01-18 02:07:09 +0200

    checking with contextconfig annotation

--- code/src/test/java/cognitivity/controllers/TestSubjectControllerTest.java
+++ code/src/test/java/cognitivity/controllers/TestSubjectControllerTest.java
@@ -15,6 +15,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {TestContextBeanConfiguration.class, HibernateBeanConfiguration.class})
+@ContextConfiguration(classes = {TestContextBeanConfiguration.class, HibernateBeanConfiguration.class})
 public class TestSubjectControllerTest implements RestControllerTest {
 
     private TestSubjectController controller;
