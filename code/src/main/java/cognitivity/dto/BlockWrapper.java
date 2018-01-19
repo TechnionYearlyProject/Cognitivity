9d4c91adb9cf33b0f9d2fa49b7ddd4ad3370a920
Guy Shemesh <guyshe@campus.technion.ac.il>
2018-01-20 01:28:42 +0200

    block Wrapper full test

--- code/src/main/java/cognitivity/dto/BlockWrapper.java
+++ code/src/main/java/cognitivity/dto/BlockWrapper.java
@@ -14,13 +14,13 @@ import java.util.List;
  */
 public class BlockWrapper {
 
-    private long id;
+    private Long id;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
