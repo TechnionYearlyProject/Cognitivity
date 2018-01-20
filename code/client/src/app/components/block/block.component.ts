b3ec26eba0110837dcf3ef1fa4896edb1ab235c4
markerlich <markerlich@github.com>
2018-01-20 13:06:24 +0200

    Fixed background

--- code/client/src/app/components/block/block.component.ts
+++ code/client/src/app/components/block/block.component.ts
@@ -112,8 +112,8 @@ export class BlockComponent implements OnInit {
   editQuestion(index: number){
     this.transferData.setData({editMode: true, value: this.questionList[index].question});
     let dialogRef = this.dialog.open(CreateQuestionComponent, {
-      height: '90%',
-      width:'80%',
+      height: '100%',
+      width:'100%',
       disableClose: true
     });
     dialogRef.afterClosed().subscribe(result => {
