70c67da5a1658ef7745daf0aa11333c0cde46196
YearlyProject <ophirk8396@gmail.com>
2018-01-20 13:00:41 +0200

    removed some unused code from controllers

--- code/src/main/java/cognitivity/controllers/AbstractRestController.java
+++ code/src/main/java/cognitivity/controllers/AbstractRestController.java
@@ -1,10 +1,5 @@
 package cognitivity.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-
 /**
  * Created by ophir on 17/12/17.
  */
@@ -16,15 +11,4 @@ public abstract class AbstractRestController<ServiceType> {
 
     public AbstractRestController(ServiceType service) { this.service = service; }
 
-    /**
-     * Error handler for backend errors - a 400 status code will be sent back, and the body
-     * of the message contains the exception text.
-     *
-     * @param exc - the exception caught
-     */
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> errorHandler(Exception exc) {
-        return new ResponseEntity<>(exc.getMessage(), HttpStatus.BAD_REQUEST);
-    }
 }
