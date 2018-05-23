32ec078876a12d223aaa45758c4b3d7e6048906d
markerlich <markerlich@github.com>
2018-05-23 15:48:40 +0300

    Frontend: Replaced the target with a class that hold the address for the target of the Http requests. closed #209. Author: Mark

--- code/client/src/app/services/database-service/index.ts
+++ code/client/src/app/services/database-service/index.ts
@@ -3,6 +3,12 @@ import { Test, Manager, Question, Block, QuestionAnswer } from '../../models';
 import { Http, Headers } from '@angular/http'
 import { RequestOptionsArgs } from '@angular/http/src/interfaces';
 
+class HttpTarget{
+    private static httpTarget : string = 'http://localhost:8181';
+    static getHttpTaraget(): string{
+        return this.httpTarget;
+    }
+}
 // Error handler class, holds behavior when errors are returned from server
 class ErrorHandler {
     static handleError(error: any) {
@@ -11,13 +17,15 @@ class ErrorHandler {
     }
 }
 
+
+
 /*
 This service connects our front-end components against the DB.
 Each function is described well by is name.
 */
 @Injectable()
 export class TestManagerService {
-
+    private target : string = HttpTarget.getHttpTaraget();
     //tuples for representing the way we send data between the front-end and DB.
     private headers = new Headers({'Content-Type': 'application/json'});
 
@@ -29,21 +37,21 @@ export class TestManagerService {
 
 
     saveTestManager(email: string): Promise<void> {
-        return this.http.post(`http://localhost:8181${this.base_mapping}/saveTestManager`, JSON.stringify({'email': email}),{headers: this.headers})
+        return this.http.post(`${this.target}${this.base_mapping}/saveTestManager`, JSON.stringify({'email': email}),{headers: this.headers})
         .toPromise()
         .then(() => null)
         .catch(ErrorHandler.handleError);
     }
 
     updateTestManager(manager: Manager): Promise<Manager> {
-        return this.http.post(`http://localhost:8181${this.base_mapping}/updateTestManager`, JSON.stringify(manager), {headers: this.headers})
+        return this.http.post(`${this.target}${this.base_mapping}/updateTestManager`, JSON.stringify(manager), {headers: this.headers})
         .toPromise()
         .then(res => res.json() as Manager)
         .catch(ErrorHandler.handleError);
     }
 
     getManagerId(email: string): Promise<number> {
-        return this.http.get(`http://localhost:8181${this.base_mapping}/findTestManagerIdByEmail?email=${email}`)
+        return this.http.get(`${this.target}${this.base_mapping}/findTestManagerIdByEmail?email=${email}`)
         .toPromise()
         .then(res => {
             if (res.status == 204)
@@ -55,14 +63,14 @@ export class TestManagerService {
     }
 
     deleteTestManager(id: number): Promise<void> {
-        return this.http.delete(`http://localhost:8181${this.base_mapping}/deleteTestManager`+'/${id}',{headers: this.headers})
+        return this.http.delete(`${this.target}${this.base_mapping}/deleteTestManager`+'/${id}',{headers: this.headers})
         .toPromise()
         .then(() => null)
         .catch(ErrorHandler.handleError);
     }
 
     getTestManager(managerId: number): Promise<any> {
-        return this.http.get(`http://localhost:8181${this.base_mapping}/findTestManagersForTestCriteria?testManagerId=${managerId}&testId=-1`)
+        return this.http.get(`${this.target}${this.base_mapping}/findTestManagersForTestCriteria?testManagerId=${managerId}&testId=-1`)
         .toPromise()
         .then(res => { if (res.text() == '') return null; return res.json()})
     }
@@ -77,36 +85,37 @@ export class SubjectService {
 @Injectable()
 export class TestService {
     //functions
+    private target : string = HttpTarget.getHttpTaraget();
     private headers = new Headers({'Content-Type': 'application/json'});
     base_mapping = '/tests';
     findTestsForTestManager(managerId: number): Promise<Test[]> {
-        return this.http.get(`http://localhost:8181${this.base_mapping}/findTestsForTestManagerWithoutQuestions?managerId=${managerId}`)
+        return this.http.get(`${this.target}${this.base_mapping}/findTestsForTestManagerWithoutQuestions?managerId=${managerId}`)
         .toPromise()
         .then(response => response.json() as Test[])
         .catch(ErrorHandler.handleError)
     }
     saveCognitiveTest(test: Test): Promise<void> {
-        return this.http.post(`http://localhost:8181${this.base_mapping}/saveCognitiveTest`,JSON.stringify(test),{headers : this.headers})
+        return this.http.post(`${this.target}${this.base_mapping}/saveCognitiveTest`,JSON.stringify(test),{headers : this.headers})
         .toPromise()
         .then(() => null)
         .catch(ErrorHandler.handleError);
     }
 
     deleteCognitiveTest(testId: number): Promise<void> {
-        return this.http.delete(`http://localhost:8181${this.base_mapping}/deleteCognitiveTest?testId=${testId}`, {headers: this.headers})
+        return this.http.delete(`${this.target}${this.base_mapping}/deleteCognitiveTest?testId=${testId}`, {headers: this.headers})
         .toPromise()
         .then(() => null)
         .catch(ErrorHandler.handleError)
     }
     updateCognitiveTest(test: Test): Promise<Test> {
-        return this.http.post(`http://localhost:8181${this.base_mapping}/updateCognitiveTest`, JSON.stringify(test), {headers: this.headers})
+        return this.http.post(`${this.target}${this.base_mapping}/updateCognitiveTest`, JSON.stringify(test), {headers: this.headers})
         .toPromise()
         .then(res => res.json() as Test)
         .catch(ErrorHandler.handleError)
     }
 
     findCognitiveTestById(testId: number) : Promise<Test> {
-        return this.http.get(`http://localhost:8181${this.base_mapping}/findCognitiveTestById?TestId=${testId}`)
+        return this.http.get(`${this.target}${this.base_mapping}/findCognitiveTestById?TestId=${testId}`)
         .toPromise()
         .then(response => response.json() as Test)
         .catch(ErrorHandler.handleError)
@@ -122,7 +131,7 @@ export class TestService {
 export class QuestionService {
     //functions
     base_mapping = '/test-questions';
-
+    private target = HttpTarget.getHttpTaraget();
     private headers = new Headers({'Content-Type': 'application/json'});
 
 
@@ -130,19 +139,19 @@ export class QuestionService {
 
     findTestQuestionsForManager(managerId: number, testId: number = null): Promise<Question[]> {
         if (testId == null) {
-            return this.http.get(`http://localhost:8181${this.base_mapping}/findTestQuestionsForTestCriteriaById?managerId=${managerId}`)
+            return this.http.get(`${this.target}${this.base_mapping}/findTestQuestionsForTestCriteriaById?managerId=${managerId}`)
             .toPromise()
             .then(res => res.json() as Question[])
             .catch(ErrorHandler.handleError);
         }
-        return this.http.get(`http://localhost:8181${this.base_mapping}/findTestQuestionsForTestCriteriaById?managerId=${managerId}&testId=${testId}`)
+        return this.http.get(`${this.target}${this.base_mapping}/findTestQuestionsForTestCriteriaById?managerId=${managerId}&testId=${testId}`)
             .toPromise()
             .then(res => res.json() as Question[])
             .catch(ErrorHandler.handleError);
     }
 
     deleteTestQuestion(questionId: number): Promise<void> {
-        return this.http.delete(`http://localhost:8181${this.base_mapping}/deleteTestQuestion?questionId=${questionId}`, {headers: this.headers})
+        return this.http.delete(`${this.target}${this.base_mapping}/deleteTestQuestion?questionId=${questionId}`, {headers: this.headers})
         .toPromise()
         .then(() => null)
         .catch(ErrorHandler.handleError);
@@ -162,13 +171,13 @@ export class QuestionService {
   @Injectable()
   export class TestAnswersService {
      base_mapping = '/test-answers';
-
+    private target : string = HttpTarget.getHttpTaraget();
      private headers = new Headers({'Content-Type': 'application/json'});
 
      constructor(private http: Http){}
 
      findAllAnswersForTest(testId: number): Promise<QuestionAnswer[]>{
-        return this.http.get(`http://localhost:8181${this.base_mapping}/findAllTestAnswersForATest?testId=${testId}`, {headers: this.headers})
+        return this.http.get(`${this.target}${this.base_mapping}/findAllTestAnswersForATest?testId=${testId}`, {headers: this.headers})
         .toPromise()
         .then(res => res.json() as QuestionAnswer[])
         .catch(ErrorHandler.handleError);
@@ -191,10 +200,10 @@ export class FileUploadService {
 
     private headers = new Headers({'Content-Type': 'application/json'});
 
-
+    private target : string = HttpTarget.getHttpTaraget();
     constructor(private http: Http) {}
     uploadCognitiveTest(test): Promise<void> {
-        return this.http.post(`http://localhost:8181${this.base_mapping}/loadFromJSONFile`,JSON.stringify(test),{headers : this.headers})
+        return this.http.post(`${this.target}${this.base_mapping}/loadFromJSONFile`,JSON.stringify(test),{headers : this.headers})
         .toPromise()
         .then(() => null)
         .catch(ErrorHandler.handleError);
