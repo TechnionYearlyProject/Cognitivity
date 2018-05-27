8da3208e4bd94bd346e6fc5162378e7f6389750a
Daniel Lyubin <dani9590@gmail.com>
2018-05-27 18:30:52 +0300

    Reverted logout in app component

--- code/client/src/app/app.component.ts
+++ code/client/src/app/app.component.ts
@@ -6,11 +6,7 @@ import { AuthService } from './services/auth-service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) {}
-  ngOnInit(): void {
-    this.authService.logout();   
-  }
+export class AppComponent {
   title = 'app';
 
 }
