d8b6bf6a1c7cf2f3dae825f73b6f23ea6113d9c5
Daniel Lyubin <dani9590@gmail.com>
2018-06-16 19:12:34 +0300

    GUI: Added time displaying for questions in result
    Important! delete from table testanswers before opening results page,
    since it's accessing json properties that do not exist in the old
    version.
    Issue #247

--- code/client/src/app/components/results-page/results-page.component.ts
+++ code/client/src/app/components/results-page/results-page.component.ts
@@ -57,9 +57,9 @@ export class ResultsPageComponent implements OnInit {
           case TypeQuestion.DrillDownQuestion:
               let answerString;
               if (answerObject.secnodaryAnswer == '-1') {
-                answerString = 'Main: ' + (parseInt(answerObject.primaryAnswer) + 1)
+                answerString = 'Main: ' + (parseInt(answerObject.finalAnswer.primaryAnswer) + 1)
               } else {
-                answerString = 'Main: ' + (parseInt(answerObject.primaryAnswer) + 1) + ' Secondary: ' + (parseInt(answerObject.secnodaryAnswer) + 1)
+                answerString = 'Main: ' + (parseInt(answerObject.finalAnswer.primaryAnswer) + 1) + ' Secondary: ' + (parseInt(answerObject.secnodaryAnswer) + 1)
               }
               questionAnswerParsed = {
                 id: questionAnswer.id,
@@ -67,7 +67,7 @@ export class ResultsPageComponent implements OnInit {
                 subject_id: questionAnswer.testSubject.id,
                 name: questionAnswer.testSubject.name,
                 question_type: typeQuestion,
-                conf_value: answerObject.confidence,
+                conf_value: answerObject.finalAnswer.confidence,
                 is_time_distraction: false,
                 changes_of_answer: 0,
                 time: 0,
@@ -82,12 +82,12 @@ export class ResultsPageComponent implements OnInit {
               subject_id: questionAnswer.testSubject.id,
               name: questionAnswer.testSubject.name,
               question_type: typeQuestion,
-              conf_value: answerObject.confidence,
+              conf_value: answerObject.finalAnswer.confidence,
               is_time_distraction: false,
               changes_of_answer: 0,
-              time: 0,
-              time_conf: 0,
-              answer: answerObject.answer
+              time: answerObject.answerTime / 1000,
+              time_conf: answerObject.confidenceTime / 1000,
+              answer: answerObject.finalAnswer.answer
             }
             break;
           default:
@@ -97,12 +97,12 @@ export class ResultsPageComponent implements OnInit {
                 subject_id: questionAnswer.testSubject.id,
                 name: questionAnswer.testSubject.name,
                 question_type: typeQuestion,
-                conf_value: answerObject.confidence,
+                conf_value: answerObject.finalAnswer.confidence,
                 is_time_distraction: false,
                 changes_of_answer: 0,
-                time: 0,
-                time_conf: 0,
-                answer: (parseInt(answerObject.answer) + 1)
+                time: answerObject.answerTime / 1000,
+              time_conf: answerObject.confidenceTime / 1000,
+                answer: (parseInt(answerObject.finalAnswer.answer) + 1)
               };
               break;
       }
